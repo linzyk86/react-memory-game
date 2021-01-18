109f3e593df209c1770aa9e8c1f9029b2ee70103
Lindsay W <lwhite5314@gmail.com>
2021-01-18 16:02:25 -0500

    styling

--- react-memory-game/src/components/Card.js
+++ react-memory-game/src/components/Card.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 
 
@@ -6,7 +6,7 @@ function Card(props) {
     let matchCards = []
     let openCards = []
 
-     function shuffleArray(array) {
+    function shuffleArray(array) {
         let i = array.length - 1;
         for (; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
@@ -20,64 +20,60 @@ function Card(props) {
 
 
     function clickHandler(e) {
-        
-        if(openCards.length === 0){
+
+        if (openCards.length === 0) {
             let card1 = e.target
             card1.classList.remove('closed')
             card1.classList.add('open')
             openCards.push(card1)
             console.log(openCards)
         }
-        else if(openCards.length ===1){
+        else if (openCards.length === 1) {
             let card2 = e.target
             card2.classList.remove('closed')
             card2.classList.add('open')
             openCards.push(card2)
             console.log(openCards)
 
-            
+
 
         }
-        if(openCards.length ===2){
+        if (openCards.length === 2) {
             let card1 = openCards[1]
             let card2 = openCards[0]
             console.log(card1.value, card2.clasList)
 
-            if(card1.classList.value===card2.classList.value){//need value for this part
+            if (card1.classList.value === card2.classList.value) {//need value for this part
                 console.log('Matched!!')
                 matchCards.push(card1, card2)
                 openCards = []
 
             }
-            else{
-                setTimeout(function(){
-                card1.classList.remove('open')///////!!!!!!works!
-                card2.classList.remove('open')
-                card1.classList.add('closed')///////!!!!!!works!
-                card2.classList.add('closed')
-                
-                },1000);
+            else {
+                setTimeout(function () {
+                    card1.classList.remove('open')
+                    card2.classList.remove('open')
+                    card1.classList.add('closed')
+                    card2.classList.add('closed')
+
+                }, 1000);
                 openCards = []
             }
-            
+            if (matchCards.length === 16) {
+                alert('You WIN!')
+
+            }
+
         }
-        
-         
-        }    
-
-
-    //figure out how to change classnames 
-
-   
-
-    
+    }
 
     return (
         <div>
             <h1>Memory Game</h1>
+            <h3> Click two cards and try for a match</h3>
             <div className='deck'>
                 {props.cards.map(card => {
-                    return <i className={`${card} closed`} onClick={clickHandler} key={card.id}/>
+                    return <i className={`${card} closed`} onClick={clickHandler} key={card.id} />
 
                 })}
 
